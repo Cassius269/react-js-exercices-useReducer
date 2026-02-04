94f981271b05b926dc4d820297b1c60ab3c5aca9
cassius269 <fahamygaston@gmail.com>
2026-02-04 19:02:08 +0100

    amélioration du design

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,3 +1,4 @@
+import styles from '../assets/styles/layouts/Counter.module.scss';
 import { useState } from "react";
 
 function Counter(){
@@ -5,20 +6,21 @@ function Counter(){
 
     return (
         <>
-            <h2 className="text-primary">Contenu du compteur</h2>
-            <article>
-                <h3 className="text-secondary">{count}</h3>
-                <button onClick={() => {
-                    if(count > 0){
-                        setCount(count-1)
-                    }
-                }}>-</button>
-                <button onClick={() => {
-                   setCount(count+1)
-                }}>+</button>
-                <button onClick={() => setCount(0)}>Reset</button>
-                <button onClick={() => setCount(count + 10)}>+10</button>
-                <button onClick={() => count > 5 ? setCount(count - 5) : setCount(0)}>-5</button>
+            <article className="border border-3 p-4 w-50 m-auto mt-5">
+                <h3 className="text-center">{count}</h3>
+                <div className={`d-flex justify-content-around mt-5 ${styles.divButtons}`}>
+                    <button className="btn" onClick={() => {
+                                        if(count > 0){
+                                            setCount(count-1)
+                                        }
+                                    }}>-</button>
+                    <button className="btn" onClick={() => {
+                                    setCount(count+1)
+                                    }}>+</button>
+                    <button className="btn" onClick={() => setCount(0)}>Reset</button>
+                    <button className="btn" onClick={() => setCount(count + 10)}>+10</button>
+                    <button className="btn" onClick={() => count > 5 ? setCount(count - 5) : setCount(0)}>-5</button>
+                </div>
             </article>
         </>
     )
